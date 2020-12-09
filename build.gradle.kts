f684127c8095ed986305cf9de228b7c919f7778b
Karl Larsaeus <karl@ninjacontrol.com>
2020-12-09 22:15:59 +0100

    Fix typo in build script

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ publishing {
     repositories {
         maven {
             name = "GitHubPackages"
-            url = uri("https://maven.pkg.github.com/karlll/asperite-reader")
+            url = uri("https://maven.pkg.github.com/karlll/aseprite-reader")
             credentials {
                 username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
                 password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
