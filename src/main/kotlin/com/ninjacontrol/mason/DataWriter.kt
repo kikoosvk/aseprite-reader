48cd2446c5ab2f6393217c94ba58f98c33216eaa
Kristian Chudjak <kristian.chudjak@siemens-healthineers.com>
2023-01-25 17:25:13 +0100

    Add Data writer methods

--- src/main/kotlin/com/ninjacontrol/mason/DataWriter.kt
+++ src/main/kotlin/com/ninjacontrol/mason/DataWriter.kt
@@ -3,7 +3,7 @@ package com.ninjacontrol.mason
 @ExperimentalUnsignedTypes
 class DataWriter() {
     private var index: Int = 0
-    val buffer = ByteArray(100)
+    val buffer = ByteArray(9999)
     val size: Int get() = buffer.size
 
     init {
@@ -17,34 +17,24 @@ class DataWriter() {
         index += amount
     }
 
-    fun writeByte(byte: aseByte){
+    fun writeByte(byte: aseByte) {
         this.buffer[index++] = byte.toByte()
     }
-    fun getByte(): aseByte = buffer[index++].toUByte()
+
     fun skipByte() = skip()
-    fun getBytes(n: Int): UByteArray {
-        val value = buffer.sliceArray(index..index + (n - 1)).asUByteArray()
-        advance(n)
-        return value
-    }
 
     fun writeBytes(array: UByteArray) {
-        array.toByteArray().copyInto(buffer, index, 0, array.size)
+        writeBytes(array.toByteArray())
+    }
+
+    fun writeBytes(array: ByteArray) {
+        array.copyInto(buffer, index, 0, array.size)
         index += array.size
     }
 
     fun skipBytes(n: Int) = skip(n)
     private fun skip(n: Int = 1) {
         index += n
-    }
-
-    fun getWord(): aseWord {
-        // save Byte to Int, shift to left + add prev. byte
-        val value: aseWord =
-            ((buffer[index + 1].toUInt() shl 8) + buffer[index].toUByte()).toUShort()
-
-        advance(2)
-        return value
     }
 
     fun writeWord(word: aseWord) {
@@ -55,41 +45,53 @@ class DataWriter() {
 
     fun skipWord() = skip(2)
 
-    fun getShort(): aseShort {
-        val value: aseShort =
-            ((buffer[index + 1].toUInt() shl 8) + buffer[index].toUByte()).toShort()
+    fun writeShort(sh1: aseShort) {
+        buffer[index] = sh1.toByte()
+        buffer[index + 1] = (sh1.toInt() shr 8).toByte()
         advance(2)
-        return value
     }
 
-    fun getDword(): aseDword {
-        val valueHigh = getWord()
-        val valueLow = getWord()
-        return ((valueLow.toUInt() shl 16) + valueHigh.toUInt())
+    fun writeDword(dword: aseDword) {
+        val valueLow = (dword shr 16).toUShort()
+        val valueHigh = dword.toUShort()
+        writeWord(valueHigh)
+        writeWord(valueLow)
     }
 
     fun skipDword() = skip(4)
 
-    fun getLong(): aseLong = getDword().toInt()
-    fun getString(): String {
-        val length: aseWord = getWord()
-        val chars = getBytes(length.toInt())
-        return chars.toByteArray().decodeToString()
+    fun writeLong(long: aseLong) {
+        writeDword(long.toUInt())
     }
 
-    // Read 16.16 fixed point value
-    fun getFixed(): Float {
-        val fixedLong = getLong()
-        return (fixedLong / 65536f)
+
+    fun writeString(str: String) {
+        val array = str.toByteArray()
+        writeWord(array.size.toUShort())
+        writeBytes(array)
     }
 
-    fun getRGBAPixel(): RGBAPixel {
-        val data = getBytes(4)
-        return RGBAPixel(red = data[0], green = data[1], blue = data[2], alpha = data[3])
+    // Write 16.16 fixed point value
+    fun writeFixed(fixed: Float) {
+        val fixedLong = fixed * 65536f
+
+        writeLong(fixedLong.toInt())
     }
 
-    fun getGrayscalePixel(): GrayscalePixel {
-        val data = getBytes(2)
-        return GrayscalePixel(value = data[0], alpha = data[1])
+
+    fun writeRGBAPixel(pixel: RGBAPixel) {
+        val (red, green, blue, alpha) = pixel
+        writeByte(red)
+        writeByte(green)
+        writeByte(blue)
+        writeByte(alpha)
     }
+
+
+    fun writeGrayscalePixel(pixel: GrayscalePixel) {
+        val (value, alpha) = pixel
+        writeByte(value)
+        writeByte(alpha)
+    }
+
 }
--- src/test/kotlin/com/ninjacontrol/mason/DataWriterTest.kt
+++ src/test/kotlin/com/ninjacontrol/mason/DataWriterTest.kt
@@ -51,7 +51,7 @@ open class DataWriterTest {
     }
 
     @Test
-    fun getWord() {
+    fun writeWord() {
         val dataWriter = DataWriter()
         val word1: aseWord = 0x01235U
         val word2: aseWord = 0x059D4U
@@ -70,5 +70,118 @@ open class DataWriterTest {
         assertEquals(b6, data.getByte())
     }
 
+    @Test
+    fun writeShort() {
+        // 0x0FFFF (65535 unsigned) in 2's complement = 0x0001 -> -1 signed
+        // 0x0FEFF (65279 unsigned) in 2's complement = 0x00101 -> -257 signed
+        val dataWriter = DataWriter()
+        val uint1: UInt = (0xFFU).toUInt()
+        val sh1 = ((uint1 shl 8) + (0xFFU).toUInt()).toShort()
+        val uini2: UInt = (0xFEU).toUInt()
+        val sh2 = ((uini2 shl 8) + (0xFFU).toUInt()).toShort()
+
+        dataWriter.writeShort(sh1)
+        dataWriter.writeShort(sh2)
+
+        val data = Data(dataWriter.buffer)
+        val s1: aseShort = data.getShort()
+        val s2: aseShort = data.getShort()
+        assertEquals(-1, s1)
+        assertEquals(-257, s2)
+    }
+
+    @Test
+    fun writeDword() {
+        val dataWriter = DataWriter()
+        val s1: aseDword = 0x012345678U // (305419896u)
+        dataWriter.writeDword(s1)
+
+        val data = Data(dataWriter.buffer)
+
+        assertEquals(s1, data.getDword())
+    }
+
+    @Test
+    fun writeLong() {
+        val dataWriter = DataWriter()
+        val s1: aseLong = -231451016 // 0xDCBA988
+        dataWriter.writeLong(s1)
+
+        val data = Data(dataWriter.buffer)
+
+        assertEquals(s1, data.getLong())
+    }
+
+    @Test
+    fun writeFixed() {
+        val dataWriter = DataWriter()
+        val s1: Float = -3531.66223144f
+        val s2: Float = 4.59999f
+        val s3: Float = 0.98f
+
+        dataWriter.writeFixed(s1)
+        dataWriter.writeFixed(s2)
+        dataWriter.writeFixed(s3)
+
+        val data = Data(dataWriter.buffer)
+
+        val returnedFixed1 = data.getFixed()
+        val returnedFixed2 = data.getFixed()
+        val returnedFixed3 = data.getFixed()
+        assertEquals(s1, returnedFixed1, 0.00001f)
+        assertEquals(s2, returnedFixed2, 0.0001f)
+        assertEquals(s3, returnedFixed3, 0.00001f)
+    }
+
+
+    @Test
+    fun writeString() {
+        val dataWriter = DataWriter()
+        val str = "detta är ett test!\"&&&&ÅÄÖ§"
+
+        dataWriter.writeString(str)
+
+        val data = Data(dataWriter.buffer)
+        val returnedStr = data.getString()
+        assertEquals(str, returnedStr)
+    }
+
+    @Test
+    fun writeStringMine() {
+        val dataWriter = DataWriter()
+        val byte: UByte = 0xFFU
+        val str = "This is a test of a write string"
+
+        dataWriter.writeByte(byte)
+        dataWriter.writeString(str)
+
+        val data = Data(dataWriter.buffer)
+        assertEquals(byte, data.getByte())
+        val returnedStr = data.getString()
+        assertEquals(str, returnedStr)
+    }
+
+
+    @Test
+    fun writeRGBAPixel() {
+        val dataWriter = DataWriter()
+        val pixel = RGBAPixel(red = 0x0FFU, green = 0x0EEu, blue = 0x0DDu, alpha = 0x099u)
+
+        dataWriter.writeRGBAPixel(pixel)
+
+        val data = Data(dataWriter.buffer)
+        assertEquals(pixel, data.getRGBAPixel())
+    }
+
+    @Test
+    fun writeGrayscalePixel() {
+        val dataWriter = DataWriter()
+        val pixel = GrayscalePixel(value = 0x0DDu, alpha = 0x099u)
+
+        dataWriter.writeGrayscalePixel(pixel)
+
+        val data = Data(dataWriter.buffer)
+        assertEquals(pixel, data.getGrayscalePixel())
+    }
 
 }
